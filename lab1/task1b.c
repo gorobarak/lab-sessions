363f17fab6885d13342382480e963f1982e02f5f
gorobarak <62811776+gorobarak@users.noreply.github.com>
2022-03-29 17:15:25 +0300

    dd

--- lab1/task1b.c
+++ lab1/task1b.c
@@ -7,6 +7,7 @@ int main(int argc, char* argv[]){
     FILE* err = stderr;
     int debug = 0;
     int i;
+    int counter = 0;
     for(i = 1; i < argc; i++){
         if (strcmp("-D", argv[i]) == 0){
             debug = 1;
@@ -21,11 +22,21 @@ int main(int argc, char* argv[]){
     while(ch != EOF){
         if ( 64 < ch && ch <91){
             fputc(46,out);// 46 == .
+            counter++;
+            if(debug){
+                fprintf(err, "%i %i", ch,46);
+            }
         }
         else{
             fputc(ch,out);
+            if(debug){
+                fprintf(err,"%i %i", ch,ch);
+            }
         }
         ch = fgetc(in);
+    }
+    if(debug){
+        fprintf(err, "the number of letters: %i", counter);
     }
     printf("/n");
     return 0;
