6197dec4f074fada7da9c0b5a35442f21110489a
gorobara <gorobarak@gmail.com>
2022-04-24 16:14:25 -0400

    finished except submission files

--- lab2/task2b.c
+++ lab2/task2b.c
@@ -2,6 +2,22 @@
 #include <stdio.h>
 #include <string.h>
 
+char censor(char c) {
+  if(c == '!')
+    return '*';
+  else
+    return c;
+}
+
+char quit(char c){
+    if(c == 'q'){
+        exit(0);
+    }
+    else{
+       return c;
+    }
+}
+
 char encrypt(char c){
     if(0x41 <= c && c <= 0x7a){
         return c + 2;
@@ -40,4 +56,11 @@ char my_get(char c){
     return ch;
 }
 
+char* map(char *array, int array_length, char (*f) (char)){
+  char* mapped_array = (char*)(malloc(array_length*sizeof(char)));
+  for (int i = 0; i < array_length; i++){
+      mapped_array[i] = f(array[i]);
+  }
+  return mapped_array;
+}
 
